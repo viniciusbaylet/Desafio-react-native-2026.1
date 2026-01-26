9f8ea979ef362b3917f127947edc9beda49a13c9
viniciusbaylet <viniciusbaylet6@gmail.com>
2026-01-26 13:58:27 -0300

    feat: cria estrutura de pastas do projeto e variaveis de estilo globais

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    blue_100: "#C2FFFE",
+    blue_200: "#0CABA8",
+    blue_300: "#008F8C",
+    blue_700: "#015958",
+    blue_900: "#023535",
+    yellow_200: "#DBF227",
+    white: "#FFFFFF",
+};
---
+++ src/styles/fonts.ts
@@ -0,0 +1,4 @@
+export const fonts = {
+    roboto: require('@/assets/fonts/Roboto-Regular.ttf'),
+    italiana: require('@/assets/fonts/Italiana-Regular.ttf'),
+}
